c07250e164150f5d77d6189b18176f81333d8a6c
PeperSauce <n540513@e1.ntps.kh.edu.tw>
2021-05-18 22:56:56 +0800

    defalut

---
+++ index.js
@@ -0,0 +1,17 @@
+try{
+  const fs = require("fs")
+  const mineflayer = require("mineflayer")
+
+  const bot = mineflayer.createBot({
+    host: "mcfallout.net",
+    username: "hackerboss32029034@gmail.com",
+    password: "sam32029034",
+  })
+
+  bot.once("spawn",() => {
+    bot.chat("嗨 YenKa305")
+  })
+}catch (error){
+  console.log(error)
+}finally{
+}
